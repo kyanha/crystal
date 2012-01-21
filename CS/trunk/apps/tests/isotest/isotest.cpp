1eb4c27d49f309327b86db8a85792cb48ce4d6cd
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-01-21 14:42:49 +0000

    Switch to DejaVu since Vera has been removed from CS in r33643

--- CS/trunk/apps/tests/isotest/isotest.cpp
+++ CS/trunk/apps/tests/isotest/isotest.cpp
@@ -328,7 +328,7 @@ bool IsoTest::SetupModules ()
   view->SetRectangle (0, 0, g2d->GetWidth (), g2d->GetHeight ());
 
   font = g3d->GetDriver2D ()->GetFontServer()->LoadFont
-    ("/fonts/ttf/Vera.ttf", 10);
+    ("/fonts/ttf/DejaVuSans.ttf", 10);
   if(!font) // fallback
     font = g3d->GetDriver2D ()->GetFontServer()->LoadFont(CSFONT_LARGE);
 
