60a5ab7d793de66690f3212af3dd72515ccf68f3
groto <groto@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-02-01 15:27:33 +0000

    Added tag 'normalmap' to textures (added the 'isNormalMap' property to 'Image' type)

--- CS/trunk/scripts/blender/io_scene_cs/io/image.py
+++ CS/trunk/scripts/blender/io_scene_cs/io/image.py
@@ -1,14 +1,35 @@
 import bpy
 
 from .util import *
+from io_scene_cs.utilities import BoolProperty
+
+
+BoolProperty(['Image'], attr="isNormalMap", name="isNormalMap", default=False)
+
+def IdentifyNormalMap(self, material):
+  for slot in material.texture_slots:
+    if slot and slot.use_map_normal and slot.texture and slot.texture.type =='IMAGE' \
+          and slot.texture.image and slot.texture.image.uname == self.uname :
+      self.isNormalMap = True   
+      return True
+  self.isNormalMap = False
+  return False
+
+bpy.types.Image.IdentifyNormalMap = IdentifyNormalMap
+
 
 def TextureAsCS(self, func, depth=0, **kwargs):
   path = 'textures/'
   if 'path' in kwargs:
     path = kwargs['path']
+  if 'material' in kwargs:
+    material = kwargs['material']
+    self.IdentifyNormalMap(material)
+
   func(' '*depth +'<texture name="%s">'%(self.uname))
   func(' '*depth +'  <file>'+path+'%s</file>'%(self.ufilename))
-  #func(' '*depth +'  <class>normalmap/heightmap</class>')
+  if self.isNormalMap:
+    func(' '*depth +'  <class>normalmap</class>')    
   if self.IsBinAlpha():
     func(' '*depth +'  <alpha><binary /></alpha>')
   func(' '*depth +'</texture>')
