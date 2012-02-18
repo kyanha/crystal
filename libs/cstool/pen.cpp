500623e7531b048f1d3bd9de00d6f99421103a11
jorrit <jorrit@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-02-18 12:30:39 +0000

    Small fix to pen line drawing in case source and destination points are equal.

--- libs/cstool/pen.cpp
+++ libs/cstool/pen.cpp
@@ -362,6 +362,7 @@ void csPen::DrawThickLines (const csArray<csPenCoordinatePair>& pairs)
 
 bool csPen::ClipLine (int& x1, int& y1, int& x2, int& y2)
 {
+  if (x1 == x2 && y1 == y2) return false;
   if (x1 < 0 && x2 < 0) return false;
   if (y1 < 0 && y2 < 0) return false;
   int sw = g2d->GetWidth ();
