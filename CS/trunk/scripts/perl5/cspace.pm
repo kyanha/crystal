2f6d897818345ca0e05981166a8a9d9b788df747
crystal-manage <crystal-manage@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-02-20 04:38:37 +0000

    master header files: Automated master header file repair. Visual-C++ project files: Automated Visual-C++ project file repair. Texinfo files: Automated Texinfo @node and @menu repair. Swig Python files: Automated Swig Python file repair. Swig Perl5 files: Automated Swig Perl5 file repair. User's Manual: Automated Texinfo to HTML conversion. csver.h: Automated csver.h regeneration.

--- CS/trunk/scripts/perl5/cspace.pm
+++ CS/trunk/scripts/perl5/cspace.pm
@@ -9879,6 +9879,7 @@ use vars qw(@ISA %OWNER %ITERATORS %BLESSEDMEMBERS);
 *GetHWRenderer = *cspacec::iGraphics2D_GetHWRenderer;
 *GetHWGLVersion = *cspacec::iGraphics2D_GetHWGLVersion;
 *GetHWVendor = *cspacec::iGraphics2D_GetHWVendor;
+*Draw3DLine = *cspacec::iGraphics2D_Draw3DLine;
 *scfGetVersion = *cspacec::iGraphics2D_scfGetVersion;
 sub DESTROY {
     return unless $_[0]->isa('HASH');
@@ -10145,6 +10146,7 @@ use vars qw(@ISA %OWNER %ITERATORS %BLESSEDMEMBERS);
 *DrawMesh = *cspacec::iGraphics3D_DrawMesh;
 *DrawSimpleMesh = *cspacec::iGraphics3D_DrawSimpleMesh;
 *DrawPixmap = *cspacec::iGraphics3D_DrawPixmap;
+*DrawLine = *cspacec::iGraphics3D_DrawLine;
 *ActivateBuffers = *cspacec::iGraphics3D_ActivateBuffers;
 *DeactivateBuffers = *cspacec::iGraphics3D_DeactivateBuffers;
 *SetTextureState = *cspacec::iGraphics3D_SetTextureState;
@@ -10189,7 +10191,6 @@ use vars qw(@ISA %OWNER %ITERATORS %BLESSEDMEMBERS);
 *GetEdgeDrawing = *cspacec::iGraphics3D_GetEdgeDrawing;
 *SetTessellation = *cspacec::iGraphics3D_SetTessellation;
 *GetTessellation = *cspacec::iGraphics3D_GetTessellation;
-*DrawLine = *cspacec::iGraphics3D_DrawLine;
 *scfGetVersion = *cspacec::iGraphics3D_scfGetVersion;
 sub DESTROY {
     return unless $_[0]->isa('HASH');
