5ee5e738e3f08c450121934f52362c34f200e101
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-02-21 14:49:42 +0000

    Added export of the 'up' and 'forward' tags for the camera positions

--- scripts/blender/io_scene_cs/io/scene.py
+++ scripts/blender/io_scene_cs/io/scene.py
@@ -1,7 +1,7 @@
 import bpy
 
 from .util import *
-
+import mathutils
 
 def ObjectHasMergingGroup(self):
   for group in self.users_group:
@@ -75,13 +75,16 @@ def CameraAsCS (self, func, depth, camera=None):
     func(' '*depth +'  <sector>%s</sector>'%(self.uname))
     func(' '*depth +'  <position x="0.0" z="0.0" y="0.0" />')
     func(' '*depth +'  <up y="1" x="0" z="0" />')
+    func(' '*depth +'  <forward y="0" x="0" z="1" />')
     func(' '*depth +'</start>')
   else:
     func(' '*depth +'<start name="%s">'%(camera.uname))
     func(' '*depth +'  <sector>%s</sector>'%(self.uname))
     # Flip Y and Z axis.
     func(' '*depth +'  <position x="%f" z="%f" y="%f" />'%tuple(camera.location))
-    func(' '*depth +'  <up y="1" x="0" z="0" />')
+    loc, rot, scale = DecomposeMatrix(camera.matrix_world)
+    func(' '*depth +'  <up y="%f" x="%f" z="%f" />'%tuple(rot * mathutils.Vector((0.0, 0.0, 1.0))))
+    func(' '*depth +'  <forward y="%f" x="%f" z="%f" />'%tuple(rot * mathutils.Vector((-1.0, 0.0, 0.0))))
     func(' '*depth +'</start>')
 
 bpy.types.Scene.CameraAsCS = CameraAsCS
