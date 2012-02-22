f8ab164a90567f53462ae7f69568644286f1ab9c
groto <groto@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-02-22 16:02:44 +0000

    Added portal support (portals are exported in the 'world' file)

--- scripts/blender/io_scene_cs/io/scene.py
+++ scripts/blender/io_scene_cs/io/scene.py
@@ -1,7 +1,8 @@
 import bpy
 
-from .util import *
 import mathutils
+from .util import *
+
 
 def ObjectHasMergingGroup(self):
   for group in self.users_group:
@@ -32,13 +33,16 @@ bpy.types.Scene.GetDependencies = SceneDependencies
 
 def SceneAsCS(self, func, depth=0):
   """ Write an xml decription of this scene: it is exported as a CS sector
-      with a reference to all objects of types mesh, armature, group and lamp
+      with a reference to all mesh, armature, group, lamp and portal objects
       contained in this scene
   """
   func(' '*depth +'<sector name="%s">'%(self.uname))
-    
-  objects = []   
-  for ob in [o for o in self.objects if o.type != 'CAMERA']:
+
+  # Export portals
+  objects = self.PortalsAsCS(func, depth)
+
+  # Export lamps and objects (as instancies of factories)
+  for ob in [o for o in self.objects if o.type != 'CAMERA']:    
     if not ob.parent or (ob.parent and ob.parent_type == 'BONE'):
       if ob not in objects:
         group = ob.hasMergingGroup()
@@ -47,10 +51,99 @@ def SceneAsCS(self, func, depth=0):
           objects.extend([object for object in group.objects])
         else:
           ob.AsCS(func, depth+2)
+          objects.append(ob)
     
   func(' '*depth +'</sector>')
 
 bpy.types.Scene.AsCS = SceneAsCS
+
+
+def GetTarget(name):
+  """ Search for the object called 'name' in the current world.
+      Return the object and the scene it belongs to if it was found; 
+      None otherwise
+  """
+  targetOb = None
+  targetSc = None
+  for sc in bpy.data.scenes:
+    for ob in sc.objects:
+      if ob.name == name:
+        targetOb = ob
+        targetSc = sc
+        break
+
+  return targetOb, targetSc
+
+
+def PortalsAsCS(self, func, depth=0):
+  """ Export portals defined in the current scene and
+      return them as a list of objects
+  """
+  epsilon = 1e-7
+  portals = []
+  for ob in [o for o in self.objects if o.type=='MESH' and o.data.portal]:
+    portals.append(ob)
+    # Check if the portal defines vertices
+    if len(ob.data.vertices) == 0:
+      print("\nWARNING: portal '%s' not exported: this mesh has no vertex"%(ob.name))
+      continue
+
+    # Find the destination mesh and scene
+    source = ob
+    destName = source.data.destinationPortal
+    destObject, destScene = GetTarget(destName)
+    if destObject == None:
+      print("\nWARNING: portal '%s' not exported: target mesh '%s' " \
+              "is not a valid object name"%(source.name,destName))
+      continue
+
+    # Export the portal
+    func(' '*depth +'  <portals name="%s">'%(ob.uname))
+    func(' '*depth +'    <portal>')
+    # Write the position of the portal (i.e. the coordinates of its vertices)
+    for v in ob.data.vertices:
+      vPos = ob.relative_matrix * v.co
+      func(' '*depth +'      <v x="%f" z="%f" y="%f" />'%(tuple(vPos)))
+    # Write the name of the destination sector
+    func(' '*depth +'      <sector>%s</sector>'%(destScene.uname))
+
+    # Evaluate the warping transform
+    # (a space warping portal changes the camera transform)
+    currPos, currMat, scale = DecomposeMatrix(source.matrix_world)
+    destPos, destMat, scale = DecomposeMatrix(destObject.matrix_world)
+    totMat = destMat * currMat.inverted()
+    destPos_after_rotate = destPos.to_4d() * totMat
+    dpos = []
+    # Vector added to the camera transform after the warping matrix is applied
+    for i in range(len(currPos)):
+      dpos.append(-(destPos_after_rotate[i]-currPos[i]))
+    if (abs(dpos[0])>epsilon or abs(dpos[1])>epsilon or abs(dpos[2])>epsilon):
+      func(' '*depth +'      <ww x="%f" y="%f" z="%f" />'%(dpos[0],dpos[2],dpos[1]))
+    # Warping matrix
+    itotMat = totMat.inverted()
+    euler = itotMat.to_euler()
+    quat = itotMat.to_quaternion()
+    if abs(quat.angle)>epsilon:
+      func(' '*depth +'      <matrix>')
+      # Order 'YZX' is important!
+      if abs(quat.axis[2])>epsilon:
+        func(' '*depth +'        <roty>%s</roty>'%(euler[2]))
+      if abs(quat.axis[1])>epsilon:
+        func(' '*depth +'        <rotz>%s</rotz>'%(-euler[1]))
+      if abs(quat.axis[0])>epsilon:
+        func(' '*depth +'        <rotx>%s</rotx>'%(-euler[0]))
+      func(' '*depth +'      </matrix>')
+
+    # Set default portal flags
+    func(' '*depth +'      <zfill />')
+    func(' '*depth +'      <float />')
+    func(' '*depth +'    </portal>')
+    func(' '*depth +'    <priority>portal</priority>')
+    func(' '*depth +'  </portals>')
+
+  return portals
+
+bpy.types.Scene.PortalsAsCS = PortalsAsCS
 
 
 def GetCameras(self):
---
+++ scripts/blender/io_scene_cs/ui/portal.py
@@ -0,0 +1,60 @@
+import bpy
+
+from io_scene_cs.utilities import rnaType, rnaOperator, B2CS, BoolProperty, StringProperty
+from . import data, settings
+
+
+@rnaType
+class MESH_PT_csPortal(bpy.types.Panel):
+  bl_space_type = "PROPERTIES"
+  bl_region_type = "WINDOW"
+  bl_context = "data"
+  b2cs_context = "data"
+  bl_label = "Crystal Space Portal Tool"
+  REMOVED = []
+
+  def draw(self, context):    
+    ob = context.active_object
+
+    if ob.type == 'MESH':
+      # Draw a checkbox to define current mesh object as a portal
+      layout = self.layout
+      row = layout.row()
+      row.prop(ob.data, "portal")
+
+      if ob.data.portal:
+          # Get the user defined name of the target mesh
+          row = layout.row()
+          split = row.split(percentage=0.5)
+          colL = split.column()
+          colL.label(text='Destination mesh:', icon='MESH_DATA')
+          colR = split.column()
+          colR.prop(ob.data, "destinationPortal", text='')
+
+          # Verify that the target mesh name corresponds 
+          # to a mesh object of the world
+          source = ob.data
+          target = source.destinationPortal
+          if target:
+              targetIsValid = False
+              for o in bpy.data.objects:
+                  if o.name == target and o.type=='MESH':
+                      targetIsValid = True
+                      break
+              # Write an error message if the given name is not valid
+              if not targetIsValid:
+                  row = colR.row()
+                  row.label(text="ERROR: unknown mesh name")
+
+
+BoolProperty(['Mesh'], 
+             attr="portal", 
+             name="Portal", 
+             description="This mesh is defined as a cs portal", 
+             default=False)
+
+StringProperty(['Mesh'], 
+               attr="destinationPortal", 
+               name="Destination portal", 
+               description="Name of the destination mesh for this portal",
+               default='')
