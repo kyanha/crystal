09f50dc3323839fe8dcfb590ba4ba3cedf28aa25
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-02-23 16:17:09 +0000

    Fixed an incorrect rotation component for the camera positions

--- scripts/blender/io_scene_cs/io/scene.py
+++ scripts/blender/io_scene_cs/io/scene.py
@@ -166,9 +166,9 @@ def CameraAsCS (self, func, depth, camera=None):
     # Set a default camera if none was found in current scene
     func(' '*depth +'<start name="Camera">')
     func(' '*depth +'  <sector>%s</sector>'%(self.uname))
-    func(' '*depth +'  <position x="0.0" z="0.0" y="0.0" />')
-    func(' '*depth +'  <up y="1" x="0" z="0" />')
-    func(' '*depth +'  <forward y="0" x="0" z="1" />')
+    func(' '*depth +'  <position x="0" y="0" z="0" />')
+    func(' '*depth +'  <up x="0" y="1" z="0" />')
+    func(' '*depth +'  <forward x="0" y="0" z="1" />')
     func(' '*depth +'</start>')
   else:
     func(' '*depth +'<start name="%s">'%(camera.uname))
@@ -176,8 +176,8 @@ def CameraAsCS (self, func, depth, camera=None):
     # Flip Y and Z axis.
     func(' '*depth +'  <position x="%f" z="%f" y="%f" />'%tuple(camera.location))
     loc, rot, scale = DecomposeMatrix(camera.matrix_world)
-    func(' '*depth +'  <up y="%f" x="%f" z="%f" />'%tuple(rot * mathutils.Vector((0.0, 0.0, 1.0))))
-    func(' '*depth +'  <forward y="%f" x="%f" z="%f" />'%tuple(rot * mathutils.Vector((-1.0, 0.0, 0.0))))
+    func(' '*depth +' <up x="%f" z="%f" y="%f" />'%tuple(rot * mathutils.Vector((0.0, 1.0, 0.0))))
+    func(' '*depth +' <forward x="%f" z="%f" y="%f" />'%tuple(rot * mathutils.Vector((0.0, 0.0, -1.0))))
     func(' '*depth +'</start>')
 
 bpy.types.Scene.CameraAsCS = CameraAsCS
