a9d7f38932e42f73e7b579d4bbf7f60851e885cb
sunshine <sunshine@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-07-17 06:57:44 +0000

    vararg: iProgressMeter::SetProgressDescription()
    iProgressMeter::SetProgressDescription(...) triggers a compilation
    error[1] on OSX.  Work around the problem by #ifdef-ing out this API and
    having clients instead invoke SetProgressDescriptionV(va_list) via
    convenience function CS::va_call().
    
    [1] Apple's llvm-g++ compiler has incomplete vararg support.  When it
    encounters a virtual method using varargs in a virtual base class, it
    aborts with error "generic thunk code fails for method foo::bar() which
    uses '...'".

--- include/ivaria/pmeter.h
+++ include/ivaria/pmeter.h
@@ -33,6 +33,8 @@
 struct iProgressMeter : public virtual iBase
 {
   SCF_INTERFACE(iProgressMeter, 2,0,0);
+
+#ifndef CS_VIRTUAL_BASE_VARARG_BROKEN
   /**
    * Set the id and description of what we are currently monitoring.
    * An id can be something like "crystalspace.engine.lighting.calculation".
@@ -40,6 +42,7 @@ struct iProgressMeter : public virtual iBase
    */
   virtual void SetProgressDescription (const char* id,
         const char* description, ...) CS_GNUC_PRINTF (3, 4) = 0;
+#endif
 
   /**
    * Set the id and description of what we are currently monitoring.
