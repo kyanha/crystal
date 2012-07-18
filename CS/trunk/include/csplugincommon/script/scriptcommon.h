06dbbf824dc7eac81d49b9df6805c28db7d7c1b2
sunshine <sunshine@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-07-18 04:03:06 +0000

    Revert "vararg: iScript::Call() and NewObject(); iScriptObject::Call()"
    Apple's llvm-gcc's support for C++ is incomplete. It throws an error
    upon encountering a virtual vararg method in a virtual base class. The
    implemented work-around for this problem is to #ifdef-out the offending
    methods and instead have clients invoke the va_list counterpart of the
    vararg method via CS::va_call(). This approach, however, is somewhat
    maintenance heavy and just plain ugly. A more maintainable and less
    invasive work-around is instead to implement the vararg methods as thin
    inline non-virtual wrappers which delegate to the va_list counterparts
    automatically.  In preparation for this revised solution, revert the
    CS::va_call() approach.

--- CS/trunk/include/csplugincommon/script/scriptcommon.h
+++ CS/trunk/include/csplugincommon/script/scriptcommon.h
@@ -52,7 +52,6 @@ public:
 
   virtual bool SetPointer (void*) { return false; }
 
-#ifndef CS_VIRTUAL_BASE_VARARG_BROKEN
   virtual bool Call (const char *name, const char *format, ...);
   virtual bool Call (const char *name, int &ret, const char *fmt, ...);
   virtual bool Call (const char *name, float &ret, const char *fmt, ...);
@@ -60,7 +59,6 @@ public:
   virtual bool Call (const char *name, csRef<iString>&, const char *fmt, ...);
   virtual bool Call (const char *name, csRef<iScriptObject>&,
     const char *fmt, ...);
-#endif
 
   virtual bool Set (const char *name, int data);
   virtual bool Set (const char *name, float data);
@@ -102,7 +100,6 @@ protected:
 public:
   virtual ~csScriptCommon() {}
 
-#ifndef CS_VIRTUAL_BASE_VARARG_BROKEN
   virtual csRef<iScriptObject> NewObject (const char *type,
     const char *ctorFormat, ...);
 
@@ -113,7 +110,6 @@ public:
   virtual bool Call (const char *name, csRef<iString>&, const char *fmt, ...);
   virtual bool Call (const char *name, csRef<iScriptObject> &ret,
     const char *fmt, ...);
-#endif
 
   virtual bool Store (const char *name, int data);
   virtual bool Store (const char *name, float data);
