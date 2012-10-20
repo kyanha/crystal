26d7343bcc2b9ad9c56697e640527b6fac016f91
weltall2 <weltall2@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-10-20 20:42:46 +0000

    ported fix to X implementation, need testing

--- CS/trunk/plugins/video/canvas/openglx/glx2d.cpp
+++ CS/trunk/plugins/video/canvas/openglx/glx2d.cpp
@@ -543,9 +543,9 @@ void csGraphics2DGLX::AllowResize (bool iAllow)
   xwin->AllowResize (iAllow);
 }
 
-bool csGraphics2DGLX::Resize (int w, int h)
+bool csGraphics2DGLX::CanvasResize (int w, int h)
 {
-  if (!csGraphics2DGLCommon::Resize (w, h)) return false;
+  if (!csGraphics2DGLCommon::CanvasResize (w, h)) return false;
   xwin->Resize (w, h);
   return true;
 }
