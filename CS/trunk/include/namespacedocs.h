0ca447c3e49512e1cb2ee589d8bcb6a2a1fe9ec7
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-03-22 10:38:22 +0000

    namespaces: Removed the CSEditing-relevant namespaces.

--- CS/trunk/include/namespacedocs.h
+++ CS/trunk/include/namespacedocs.h
@@ -78,25 +78,6 @@ namespace CS
   {}
 
   /**
-   * General tools for the edition of Crystal Space objects.
-   */
-  namespace Editor
-  {}
-
-  /**
-   * Objects and tools specific to the Crystal Space editor. Most of
-   * these tools need the wxWidgets dependency.
-   */
-  namespace EditorApp
-  {
-    /**
-     * Default components for the cseditor framework.
-     */
-    namespace Component
-    {}
-  }
-
-  /**
    * Geometry and mesh tools related classes.
    */
   namespace Geometry
