5d5e688f5292a7c2cda4432922c1c5c4ccc72afc
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-07-05 13:33:53 +0000

    iModifiable and related: - Renamed more consistently most classes from "cstool/modifiableimpl.h". - API doc improvements - Misc tweaks

--- CS/trunk/include/iutil/modifiable.h
+++ CS/trunk/include/iutil/modifiable.h
@@ -35,35 +35,44 @@ struct iModifiableDescription;
 //----------------- iModifiableParameter ---------------------
 
 /**
- * @TODO
+ * Description of a specific parameter of a iMovable.
+ * \sa BaseModifiableParameter for a default implementation
  */
 struct iModifiableParameter : public virtual iBase
 {
   SCF_INTERFACE (iModifiableParameter, 1, 0, 0);
 
-  /// Return the ID of this parameter
+  /**
+   * Get the ID of this parameter. This ID is built from the string returned by
+   * GetLabel()
+   */
   virtual csStringID GetID () const = 0;
+
+  /**
+   * Get the label of this parameter. A label identifies (supposedly uniquely)
+   * this parameter.
+   */
   virtual const char* GetLabel () const = 0;
 
   /**
-   * Return the parameter's name
-   * to be processed by the translator.
+   * Get the parameter's name
+   * \note You might want to process this string by the translator.
    */
   virtual const char* GetName () const = 0;
 
   /**
-   * Return the parameter's textual description
-   * to be processed by the translator.
+   * Get the parameter's textual description
+   * \note You might want to process this string by the translator.
    */
   virtual const char* GetDescription () const = 0;
 
   /**
-   * Return the type of this parameter
+   * Get the type of this parameter
    */
   virtual csVariantType GetType () const = 0;
 
   /**
-   * Return this parameter's constraint, or nullptr if there are none.
+   * Get this parameter's constraint, or nullptr if there are none.
    */
   virtual iModifiableConstraint* GetConstraint () const = 0;
 };
@@ -71,36 +80,51 @@ struct iModifiableParameter : public virtual iBase
 //----------------- iModifiableDescription ---------------------
 
 /**
- * The descriptor of an iModifiable object. Contains ways to expose and access its
- * properties.
+ * The descriptor of an CS::Utility::iModifiable object. It contains all the
+ * information needed in order to expose and access the properties of a iModifiable.
+ *
+ * It can be used fo an automated access to an object, eg in order to generate a
+ * graphical user interface (see the CSEditing library), to animate it over time, or
+ * to save and load it.
  * 
- * \see csModifiableDescription for an implementation
+ * A iModifiableDescription is created through iModifiable::GetDescription() if a user
+ * application would like to access the object without knowing its external interface.
+ * The iModifiableDescription will then allow to know how the object should be accessed.
+ *
+ * \sa BaseModifiableDescription for a default implementation
  */
 struct iModifiableDescription : public virtual iBase
 {
   SCF_INTERFACE (iModifiableDescription, 1, 0, 0);
 
-  //virtual csStringID GetID () const = 0;
+  /**
+   * Get the label of this modifiable. A label identifies (supposedly uniquely) the
+   * description of the modifiable.
+   */
   virtual const char* GetLabel () const = 0;
 
   /**
    * Get the name of this modifiable
+   * \note You might want to process this string by the translator.
    */
   virtual const char* GetName () const = 0;
 
   /**
-   * Get the number of editable parameters of this description.
+   * Get the number of editable parameters of this description. This won't include
+   * the parameters of the child descriptions.
+   * \sa GetTotalParameterCount(), GetChild()
    */
   virtual size_t GetParameterCount () const = 0;
 
   /**
    * Get the total number of editable parameters of this description, that is
    * including the parameters of all children and grand-children.
+   * \sa GetParameterCount(), GetChild()
    */
   virtual size_t GetTotalParameterCount () const = 0;
 
   /**
-   * Get a parameter based on its csStringID.
+   * Get a parameter based on its ID.
    */
   virtual iModifiableParameter* GetParameter (csStringID id) const = 0;
 
@@ -109,20 +133,47 @@ struct iModifiableDescription : public virtual iBase
    */
   virtual iModifiableParameter* GetParameter (size_t index) const = 0;
 
+  /**
+   * Find the index of a parameter based on its ID.
+   * \return The index of the parameter, or (size_t) ~0 if it was not found.
+   */
   virtual size_t FindParameter (csStringID id) const = 0;
 
+  /**
+   * Get the count of child descriptions. Child descriptions forms a hierarchical
+   * structure of sets of parameters.
+   */
   virtual size_t GetChildrenCount () const = 0;
+
+  /**
+   * Get the child description with the given index.
+   */
   virtual iModifiableDescription* GetChild (size_t index) const = 0;
 
+  /**
+   * Get the list of resource entries of this description. Each entry is a path
+   * containing additional data such as the translation files of this description.
+   */
   virtual iStringArray* GetResources () const = 0;
 };
 
 //----------------- iModifiableListener ---------------------
 
+/**
+ * Listener for the notification of the state changes of a CS::Utility::iModifiable
+ */
 struct iModifiableListener : public virtual iBase
 {
   SCF_INTERFACE (iModifiableListener, 1, 0, 0);
 
+  /**
+   * The value of the parameter of the given CS::Utility::iModifiable has changed.
+   * \param modifiable The modifiable who's value has changed.
+   * \param parameterIndex The index of the parameter that has changed.
+   *
+   * \note The new value of the parameter can be found eg through
+   * CS::Utility::iModifiable::GetParameterValue().
+   */
   // TODO: return bool to veto?
   virtual void ValueChanged (iModifiable* modifiable, size_t parameterIndex) = 0;
 };
@@ -143,19 +194,11 @@ struct iModifiableListener : public virtual iBase
  * gets set
  *
  * \see iModifiableDescription
- * \see csBasicModifiableDescription 
  * \see iModifiableParameter
- * \see csBasicModifiableParameter
  */
 struct iModifiable : public virtual iBase 
 {
   SCF_INTERFACE (iModifiable, 1, 0 ,0);
-
-  /**
-   * Returns this object's unique ID. If two objects have the same ID, then
-   * they share the same set of interfaces, hence the same iModifiableDescription.
-   */
-  //virtual const csStringID GetID () const = 0;
 
   /**
    * Returns this object's description.
@@ -185,12 +228,20 @@ struct iModifiable : public virtual iBase
    */
   virtual size_t GetTotalParameterCount () const = 0;
 
+  /**
+   * Add a listener to the list.
+   */
   virtual void AddListener (iModifiableListener* listener) = 0;
+
+  /**
+   * Remove the given listener from the list.
+   */
   virtual void RemoveListener (iModifiableListener* listener) = 0;
 };
 
 //----------------- iModifiableConstraintType ---------------------
 
+// TODO: doc
 enum iModifiableConstraintType
 {
   MODIFIABLE_CONSTRAINT_BOUNDED = 0,
@@ -200,8 +251,8 @@ enum iModifiableConstraintType
   MODIFIABLE_CONSTRAINT_VFS_PATH,
   MODIFIABLE_CONSTRAINT_TEXT_ENTRY,
   MODIFIABLE_CONSTRAINT_TEXT_BLOB,
-  MODIFIABLE_CONSTRAINT_BITMASK,
-  MODIFIABLE_CONSTRAINT_ARRAY_STATIC
+  MODIFIABLE_CONSTRAINT_BITMASK
+  //MODIFIABLE_CONSTRAINT_ARRAY_STATIC
 };
 
 //----------------- iModifiableConstraint ---------------------
@@ -257,6 +308,7 @@ struct iModifiableConstraintBounded : public virtual iModifiableConstraint
 struct iModifiableConstraintEnum : public virtual iModifiableConstraint
 {
   virtual size_t GetValueCount () const = 0;
+  // TODO: return csVariant
   virtual long GetValue (size_t index) const = 0;
   virtual const char* GetLabel (size_t index) const = 0;
 };
