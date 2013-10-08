a8fd2510d773f4f3c152197745f08d4084b2152e
sueastside <sueastside@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-10-08 15:05:03 +0000

    Changed heightmap generation to mapo's technique.

--- CS/trunk/scripts/blender/io_scene_cs/io/object_terrain.py
+++ CS/trunk/scripts/blender/io_scene_cs/io/object_terrain.py
@@ -1,4 +1,5 @@
 import bpy
+
 import mathutils
 from .util import *
 from .transform import *
@@ -44,6 +45,10 @@ class TerrainAlpaMap(object):
       if slot.use_stencil:
         stencils.append((slot, tex))
         
+    if len(stencils) == 0:
+      print('No stencils!')
+      return
+    
     if self.i > 1:
       offset = self.i*4
       stencils = stencils[offset:offset+4]
@@ -96,11 +101,12 @@ class TerrainMaterial(object):
     stencils = []
     for i, tex in enumerate(self.material.textures):
       slot = self.material.texture_slots[i]
-      if not slot.use_stencil:
-        xml.shadervar({'name': 'tex diffuse '+str(nr), 'type': 'texture'}, tex.image.uname, close=True)
-        nr += 1
-      else:
-        stencils.append((slot, tex))
+      if tex.type == 'IMAGE':
+        if not slot.use_stencil:
+          xml.shadervar({'name': 'tex diffuse '+str(nr), 'type': 'texture'}, tex.image.uname, close=True)
+          nr += 1
+        else:
+          stencils.append((slot, tex))
     
     nr = 1    
     for i in range( math.ceil((len(stencils)+1)/4) ):
@@ -116,10 +122,11 @@ class TerrainMaterial(object):
     stencils = []
     for i, tex in enumerate(self.material.textures):
       slot = self.material.texture_slots[i]
-      if not slot.use_stencil:
-        dependencies['T'][tex.image.uname] = tex.image
-      else:
-        stencils.append((slot, tex))
+      if tex.type == 'IMAGE':
+        if not slot.use_stencil:
+          dependencies['T'][tex.image.uname] = tex.image
+        else:
+          stencils.append((slot, tex))
     
     for i in range( math.ceil((len(stencils)+1)/4) ):
       map = TerrainAlpaMap(self.material, i)
@@ -162,7 +169,7 @@ def WriteCSTerrain (self, func, depth, path):
             .cells()\
                 .celldefault()\
                     .gridsize({'width': 257, 'height': 257}, close=True)\
-                    .size({'x': 256, 'y': 32, 'z': 256}, close=True)\
+                    .size({'x': 256, 'y': 1, 'z': 256}, close=True)\
                 .close()\
                 \
                 .cell()\
@@ -209,157 +216,98 @@ bpy.types.Object.TerrainAsCS = TerrainAsCS
 
 #==================================================================
 def exportHeightMap(object):
-  import time
-  mesh = object.data
-  init_time = time.time()
-  # find bounding box
-  bbox = object.bound_box
-  min = list(bbox[0])
-  max = list(bbox[0])
-  for vert in bbox:
-    for a in range(3):
-      if vert[a] < min[a]:
-        min[a] = vert[a]
-      elif vert[a] > max[a]:
-        max[a] = vert[a]
-  size = object.dimensions
-  location = object.location
-  for a in range(3):
-    min[a] = (min[a]-location[a])/size[a]
-    max[a] = (max[a]-location[a])/size[a]
-    
-  resolution = 512
-  
-  mesh.update(calc_edges=False, calc_tessface=True)
-
-  pixels = [0]*((resolution+1) * (resolution+1) * 4)
-  for face in mesh.tessfaces:
-    distributeFace(resolution, pixels, mesh,face,min,max)
-    #break
-  
   path = 'textures/'+object.uname+'-heighmap.png'
   print(' '+path)
   
-  image_object = bpy.data.images.new(name=object.uname+'-hm', width=resolution+1, height=resolution+1, alpha=False, float_buffer=False)
-  image_object.pixels = pixels
-  image_object.update()  
-  image_object.filepath_raw = GetExportPath()+path
-  image_object.save()
-  bpy.data.images.remove(image_object)
-  del image_object
+  cleanup = True
+  heightmap_size = 257 #TODO
   
+  # Make render scene
+  render_scene = bpy.data.scenes.new(object.uname+'_'+'terrain_render_scene') 
+  render_scene.render.resolution_x=heightmap_size
+  render_scene.render.resolution_y=heightmap_size
+  render_scene.render.resolution_percentage=100
+  render_scene['newScene'] = False #Make sure the handelers don't reset the engine.
+  render_scene.render.engine = 'BLENDER_RENDER'
+  render_scene.render.filepath = GetExportPath()+path
+  
+  # Copy terrain object
+  terrain = object.copy()
+  render_scene.objects.link(terrain)
+  
+  material_slots_materials = [None]*len(terrain.material_slots)
+  # Remove materials but store them
+  for i in range(len(terrain.material_slots)):
+    material_slots_materials[i] = terrain.material_slots[i].material
+    terrain.material_slots[i].material = None
+    
+  # Create Material
+  # The texture
+  hm_tex=bpy.data.textures.new(name=object.uname+'_'+"hm_tex", type="BLEND")
+  hm_tex.use_color_ramp=True
+  hm_tex.color_ramp.interpolation="LINEAR"
+  hm_tex.color_ramp.elements[0].color=(1, 1, 1, 1)
+  hm_tex.color_ramp.elements[1].color=(0, 0, 0, 1)
+  hm_tex.use_flip_axis="VERTICAL"
+
+  # The material
+  hm_mat=bpy.data.materials.new(name=object.uname+'_'+"hm_mat")
+  hm_mat.use_shadeless=True
+  hm_tslot=hm_mat.texture_slots.add()
+  hm_tslot.texture=hm_tex
+  hm_tslot.mapping="FLAT"
+  hm_tslot.mapping_x="Z"
+  hm_tslot.mapping_y="Z"
+  hm_tslot.mapping_z="Z"
+  
+  # Assign
+  #bpy.ops.object.material_slot_add() #This will fail, so lets assume the user has already added a material slot
+  terrain.material_slots[0].material=hm_mat
+  
+  
+  odim=terrain.dimensions
+  oloc=terrain.location
+  
+  # Create render camera
+  # Data
+  hm_camera=bpy.data.cameras.new(name=object.uname+'_'+"hm_camera")
+  hm_camera.type="ORTHO"
+  hm_camera.ortho_scale=max(odim[0:1]) # max of the two dimensions of the outdoor mesh
+  hm_camera.clip_start=0.001
+  hm_camera.clip_end=1.5*(oloc[2]+odim[2])
+  # Object
+  hm_camera_ob=bpy.data.objects.new(object.uname+'_'+"hm_camera", hm_camera)
+  hm_camera_ob.location=(oloc[0], oloc[1], oloc[2]+odim[2])
+  # add to scene
+  render_scene.objects.link(hm_camera_ob)
+  render_scene.camera=hm_camera_ob
+  
+  # create the image
+  render_scene.update()
+  bpy.ops.render.render(write_still=True, scene=render_scene.name)
+  #bpy.data.images['Render Result'].file_format='PNG'
+  #bpy.data.images['Render Result'].save_render(filepath='/tmp/hm2.png') 
+
+  
+  if cleanup:
+    #Cleanup
+    for i in range(len(material_slots_materials)):
+      terrain.material_slots[i].material=material_slots_materials[i]
+      
+    bpy.data.scenes.remove(render_scene)
+    bpy.data.objects.remove(hm_camera_ob)
+    bpy.data.cameras.remove(hm_camera)
+    bpy.data.materials.remove(hm_mat)
+    bpy.data.textures.remove(hm_tex)
+
   return path
 
-#==================================================================
-def getTriBounds(tri):
-  min = [tri[0].co[0],tri[0].co[1],tri[0].co[2]]
-  max = list(min)
-  for j in range(1,3):
-    vertex = tri[j] # skip the first vertex
-    for i in range(0,3):
-      if vertex.co[i] < min[i]:
-        min[i] = vertex.co[i]
-      elif vertex.co[i] > max[i]:
-        max[i] = vertex.co[i]
-  return (min,max)
-#==================================================================
-def cross_product2(v, w):
-	return (v[0]*w[1]) - (v[1]*w[0])
-  
-def pointInTri(p,tri,min,max):
-  a = tri[0].co
-  b = tri[1].co
-  c = tri[2].co
-  s1 = cross_product2(c-a,(p[0]-a[0],p[1]-a[1]))
-  s2 = cross_product2(a-b,(p[0]-b[0],p[1]-b[1]))
-  if (s1*s2 < 0): # fast skip if its already outside
-    return False
-  s3 = cross_product2(b-c,(p[0]-c[0],p[1]-c[1]))
-  if (s2*s3 < 0):
-    return False
-  return True
-  
-def getPointTriIntersection(point,tri):
-  ab = (tri[1].co - tri[0].co)
-  ac = (tri[2].co - tri[0].co)
-  
-  #PLANE = CrossVecs(ab,ac)
-  ab = mathutils.Vector(ab)
-  ac = mathutils.Vector(ac)
-  PLANE = ab.cross(ac)
-  
-  #D = -DotVecs(PLANE,tri[0].co)
-  D = - PLANE.dot(tri[0].co)
-  z = -(point[0]*PLANE[0]+(point[1]*PLANE[1]+D))/PLANE[2]
-  return z
-  
-def makeTri(mesh,face,skip):
-  tri = ()
-  for i in range(0,4):
-    if (not i == skip):
-      tri = tri + (mesh.vertices[face.vertices[i]],)
-  return tri
-#==================================================================
 
-def distributeTri(terrain_resolution, new_pixels, tri, dx, dy,min,max,trinormals):
-  def hputpixel(coords, color):
-    x, y = coords
-    print('    %f - %f '%(x, y))
-    offset = (x + (y * (terrain_resolution+1))) * 4
-    new_pixels[offset] = color[0]
-    new_pixels[offset+1] = color[1]
-    new_pixels[offset+2] = color[2]
-    new_pixels[offset+3] = 1
-  
-  box = getTriBounds(tri)
-  print('min', min, dy)
-  print('getTriBounds', box)
-  box_x1 = ((box[0][0]-min[0])/dx)
-  box_y1 = ((box[0][1]-min[1])/dy)
-  box_x2 = ((box[1][0]-min[0])/dx) + 1
-  box_y2 = ((box[1][1]-min[1])/dy) + 1
-  print('box_y1', box_y1)
-  boundsz = max[2]-min[2]
-  # check to prevent divide by 0
-  if boundsz == 0:
-    boundsz = 1.0/255.0
-  if (box_x1 < 0):
-    box_x1 = 0
-  if (box_y1 < 0):
-    box_y1 = 0
-  if (box_x2 > terrain_resolution+1):
-    box_x2 = terrain_resolution+1
-  if (box_y2 > terrain_resolution+1):
-    box_y2 = terrain_resolution+1
-  f=122.5
-  fmax = 16777215.0
-  tfactor = fmax/boundsz
-  print ('range %f - %f '%(box_y1, box_y2))
-  for y in range(int(box_y1),int(box_y2)):
-    for x in range(int(box_x1),int(box_x2)):
-      point = (x*dx + min[0], y*dy + min[1])
-      if pointInTri(point,tri,min,max):
-        val_z = getPointTriIntersection(point,tri)
-        h = (val_z-min[2])*(tfactor)
-        r = int(h)>>16
-        postr = h-(r*65536.0)
-        g = int(postr)>>8
-        b = int(postr-(g*256.0))
-        hputpixel((x,terrain_resolution-y),(r,g,b))
-#==================================================================
-def distributeFace(terrain_resolution, new_pixels, mesh,face,min,max):
-  spanx = max[0] - min[0]
-  spany = max[1] - min[1]
-  stepx = spanx / terrain_resolution
-  stepy = spany / terrain_resolution
-  
-  print('%f - %f    %f - %f'%(spanx, spany, stepx, stepy))
-  
-  if (len(face.vertices) ==4):
-    tri1 = makeTri(mesh, face,3)
-    distributeTri(terrain_resolution, new_pixels, tri1,stepx,stepy,min,max,face.normal)
-    tri2 = makeTri(mesh, face,1)
-    distributeTri(terrain_resolution, new_pixels, tri2,stepx,stepy,min,max,face.normal)
-  else:
-    tri1 = makeTri(mesh, face,3)
-    distributeTri(terrain_resolution, new_pixels, tri1,stepx,stepy,min,max,face.normal)
+
+
+if __name__ == '__main__':
+  print('---------------------------------------')
+  original_terrain = bpy.data.objects['Plane']  
+  exportHeightMap(original_terrain)
+  print('---------------------------------------')
+
