08d6a5c9ac1ea553a4378521194f3e8dbec98791
sunshine <sunshine@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-10-17 22:29:13 +0000

    scripts/blender/io_scene_cs: improve PEP8 conformance
    Primarily a whitespace-adjustment patch. Provided by Andrea Palazzi
    <palazziandrea@yahoo.it>. Resolves #998.

--- scripts/blender/io_scene_cs/ui/layout.py
+++ scripts/blender/io_scene_cs/ui/layout.py
@@ -2,14 +2,16 @@ import bpy
 
 from io_scene_cs.ui.idref import get_idtype_list_prop
 
+
 def ObjectReferenceLayout(self, data, prop, name=""):
-  box = self.box()
-  row = box.row()
-  row.label(name)
-  idtype = getattr(data, "%s__idtype__" % prop)
-  row.prop_search(data, "%s__name__" % prop, bpy.data, get_idtype_list_prop(idtype), text="")
-  
-  '''
+    box = self.box()
+    row = box.row()
+    row.label(name)
+    idtype = getattr(data, "%s__idtype__" % prop)
+    row.prop_search(data, "%s__name__" %
+                    prop, bpy.data, get_idtype_list_prop(idtype), text="")
+
+    '''
   if mesh:
     row.context_pointer_set("current_viscullmesh", mesh)
     #row.label(mesh.name)
